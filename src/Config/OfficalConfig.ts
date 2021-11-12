925b4b2bfefd23aa357c9ec17f06f4f8a0612841
mumagege <chengang01@live.com>
2021-11-12 13:46:03 +0800

    Unified to lowercase

--- src/Config/OfficalConfig.ts
+++ src/Config/OfficalConfig.ts
@@ -1,5 +1,4 @@
 import { FrameworkConfig } from "cgserver/lib/Framework/Config/FrameworkConfig";
-import { ServerConfig } from "cgserver/lib/Framework/Config/ServerConfig";
 
 export let GOfficalCfg:OfficalConfig = null
 class OfficalConfig extends FrameworkConfig
